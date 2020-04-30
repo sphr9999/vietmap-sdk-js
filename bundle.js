db404c5f8ebcfa7ef1010f1fe85a75f490e06760
tung nguyen thanh <20145117@student.hust.edu.vn>
2020-04-30 16:14:29 +0700

    init

---
+++ bundle.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var browserClient = require('./lib/browser/browser-client');
+var mbxDatasets = require('./services/datasets');
+var mbxDirections = require('./services/directions');
+var mbxGeocoding = require('./services/geocoding');
+var mbxMapMatching = require('./services/map-matching');
+var mbxMatrix = require('./services/matrix');
+var mbxOptimization = require('./services/optimization');
+var mbxStatic = require('./services/static');
+var mbxStyles = require('./services/styles');
+var mbxTilequery = require('./services/tilequery');
+var mbxTilesets = require('./services/tilesets');
+var mbxTokens = require('./services/tokens');
+var mbxUploads = require('./services/uploads');
+var mbxIsochrone = require('./services/isochrone');
+
+function mapboxSdk(options) {
+  var client = browserClient(options);
+
+  client.datasets = mbxDatasets(client);
+  client.directions = mbxDirections(client);
+  client.geocoding = mbxGeocoding(client);
+  client.mapMatching = mbxMapMatching(client);
+  client.matrix = mbxMatrix(client);
+  client.optimization = mbxOptimization(client);
+  client.static = mbxStatic(client);
+  client.styles = mbxStyles(client);
+  client.tilequery = mbxTilequery(client);
+  client.tilesets = mbxTilesets(client);
+  client.tokens = mbxTokens(client);
+  client.uploads = mbxUploads(client);
+  client.isochrone = mbxIsochrone(client);
+
+  return client;
+}
+
+module.exports = mapboxSdk;
---
+++ test/try-browser/try-browser.js
@@ -0,0 +1,73 @@
+/* eslint-disable no-console */
+'use strict';
+
+var mbxClient = require('../..');
+var mbxStyles = require('../../services/styles');
+var mbxTilesets = require('../../services/tilesets');
+var mbxTokens = require('../../services/tokens');
+var mbxDatasets = require('../../services/datasets');
+var mbxTilequery = require('../../services/tilequery');
+var mbxDirections = require('../../services/directions');
+var mbxMapMatching = require('../../services/map-matching');
+var mbxMatrix = require('../../services/matrix');
+var mbxUploads = require('../../services/uploads');
+var mbxGeocoding = require('../../services/geocoding');
+var mbxStatic = require('../../services/static');
+
+window.tryServiceMethod = function(
+  serviceName,
+  methodName,
+  config,
+  accessToken,
+  callback
+) {
+  config = config || {};
+  accessToken = accessToken || window.MAPBOX_ACCESS_TOKEN;
+
+  if (!accessToken) {
+    throw new Error('ERROR: Could not find an access token');
+  }
+
+  if (!serviceName || !methodName) {
+    throw new Error('ERROR: You must provide a service and method');
+  }
+
+  var baseClient = mbxClient({ accessToken: accessToken });
+  var services = {
+    datasets: mbxDatasets(baseClient),
+    directions: mbxDirections(baseClient),
+    geocoding: mbxGeocoding(baseClient),
+    mapMatching: mbxMapMatching(baseClient),
+    matrix: mbxMatrix(baseClient),
+    static: mbxStatic(baseClient),
+    styles: mbxStyles(baseClient),
+    tilequery: mbxTilequery(baseClient),
+    tilesets: mbxTilesets(baseClient),
+    tokens: mbxTokens(baseClient),
+    uploads: mbxUploads(baseClient)
+  };
+
+  var service = services[serviceName];
+  if (!service) {
+    throw new Error('Unknown service "' + serviceName + '"');
+  }
+
+  var method = service[methodName];
+  if (!method) {
+    throw new Error(
+      'Unknown method "' + methodName + '" for service "' + serviceName + '"'
+    );
+  }
+
+  service[methodName](config)
+    .send()
+    .then(
+      function(response) {
+        console.log(response);
+        if (callback) callback(response);
+      },
+      function(error) {
+        console.error(error);
+      }
+    );
+};
